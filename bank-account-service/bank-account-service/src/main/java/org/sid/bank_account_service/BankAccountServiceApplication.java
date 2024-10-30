e0071ffb72b7ea636b5e38789360b8466ae078a4
mehdisaih <110493586+mehdisaih@users.noreply.github.com>
2024-10-30 13:52:11 +0100

    second commit

--- bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/BankAccountServiceApplication.java
+++ bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/BankAccountServiceApplication.java
@@ -1,13 +1,40 @@
 package org.sid.bank_account_service;
 
+import org.sid.bank_account_service.Entities.BankAccount;
+import org.sid.bank_account_service.Enum.AccountType;
+import org.sid.bank_account_service.Repository.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BankAccountServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BankAccountServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(BankAccountRepository BankAccountRepository) {
+		return args -> {
+
+			for (int i = 0; i < 10; i++) {
+				BankAccount bankAccount = BankAccount.builder()
+						.id(UUID.randomUUID().toString())
+						.Balance(1000 + Math.random() * 9000)
+						.CreatedAT(new Date())
+						.currency("MAD")
+						.type(Math.random() > 0.5 ? AccountType.CURRENT_ACCOUNT : AccountType.SAVING_ACCOUNT)
+						.build();
+				BankAccountRepository.save(bankAccount);
+			}
+
+		};
+
+	}
 }
--- bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/Entities/BankAccount.java
+++ bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/Entities/BankAccount.java
@@ -1,14 +1,23 @@
 package org.sid.bank_account_service.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.sid.bank_account_service.Enum.AccountType;
 
 import java.util.Date;
-
+@Entity@NoArgsConstructor@AllArgsConstructor @Builder
 public class BankAccount {
+    @Id
     private String id ;
     private Date CreatedAT;
     private double Balance ;
     private String currency;
+    @Enumerated(EnumType.STRING)
     private AccountType type;
 
 }
