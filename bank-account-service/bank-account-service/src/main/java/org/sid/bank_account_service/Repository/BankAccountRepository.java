b00851041247a1eae4c02c0bbd1dc42231a57504
mehdisaih <110493586+mehdisaih@users.noreply.github.com>
2024-11-20 12:01:08 +0100

    spring data rest

--- bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/Repository/BankAccountRepository.java
+++ bank-account-service/bank-account-service/src/main/java/org/sid/bank_account_service/Repository/BankAccountRepository.java
@@ -2,9 +2,10 @@ package org.sid.bank_account_service.Repository;
 
 import org.sid.bank_account_service.Entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
 
 
